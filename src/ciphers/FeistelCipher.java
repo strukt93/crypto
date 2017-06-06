9c5fbb16793296fdce148a66fa4aef6b83eea850
strukt93 <strukt93@gmail.com>
2017-06-07 01:54:35 +0200

    Some code modifications

--- src/ciphers/FeistelCipher.java
+++ src/ciphers/FeistelCipher.java
@@ -2,55 +2,40 @@ package ciphers;
 
 import java.security.SecureRandom;
 import java.util.ArrayList;
-import java.util.Stack;
+
+import data.Binary;
 
 public class FeistelCipher {
 	String plainText;
 	String cipherText;
 	String key;
-	Stack<String> keys;
+	String subKey;
 	ArrayList<String> blocks;
 
 	public FeistelCipher(String plainText) {
 		this.plainText = plainText;
 		cipherText = "";
-		// key = generateKey();
-		keys = new Stack<String>();
+		key = generateKey();
+		subKey = key;
 		blocks = new ArrayList<String>();
 		generateBlocks();
 	}
 
-	public String generateSubKey() {
-		String subKey = xor(key, generateKey());
-		keys.push(subKey);
-		return subKey;
-	}
-
-	public String padWithZeros(String binary) {
-		while (binary.length() < 32)
-			binary += "0";
-		return binary;
+	public void generateSubKey() {
+		subKey = Binary.circularShiftLeft(subKey);
 	}
 
 	public void generateBlocks() {
 		String bytes = "" + plainText.charAt(0);
 		for (int i = 1; i < plainText.length(); i++) {
 			if (i % 4 == 0) {
-				String binary = toBinary(bytes);
+				String binary = Binary.toBinary(bytes);
 				blocks.add(binary);
-				// blocks.add((binary.length() < 32) ? "0" + binary : binary);
 				bytes = "";
 			}
 			bytes += plainText.charAt(i);
 		}
-		blocks.add(padWithZeros(toBinary(bytes)));
-		String out = "";
-		for (int j = 0; j < blocks.size(); j++) {
-			String x = blocks.get(j);
-			System.out.println(x);
-			out += toBytes(x);
-		}
-		System.out.println(out);
+		blocks.add(Binary.padWithZeros(Binary.toBinary(bytes), 32, false));
 	}
 
 	public String generateKey() {
@@ -60,48 +45,6 @@ public class FeistelCipher {
 			s += sr.nextInt(2);
 		}
 		return s;
-	}
-
-	public String xor(String x, String y) {
-		String z = "";
-		for (int i = 0; i < x.length(); i++) {
-			char c1 = x.charAt(i);
-			char c2 = y.charAt(i);
-			z += (c1 == c2) ? "0" : "1";
-		}
-		return z;
-	}
-
-	public String toBytes(String binary) {
-		String ret = "";
-		for (int i = 0; i < 4; i++) {
-			int val = Integer.parseInt(binary.substring(8 * i, (i + 1) * 8), 2);
-			ret += (char) val;
-		}
-		return ret;
-	}
-
-	public String toBinary(String s) {
-		byte[] bytes = s.getBytes();
-		String out = "";
-		for (byte b : bytes) {
-			String bString = Integer.toBinaryString(b);
-			while (bString.length() < 8)
-				bString = "0" + bString;
-			out += bString;
-		}
-		return out;
-	}
-
-	public String toBinary(byte[] bytes) {
-		String out = "";
-		for (byte b : bytes) {
-			String bString = Integer.toBinaryString(b);
-			if (bString.length() < 8)
-				bString = "0" + bString;
-			out += bString;
-		}
-		return out;
 	}
 
 	public static void main(String[] args) {
---
+++ src/data/Binary.java
@@ -0,0 +1,94 @@
+package data;
+
+public class Binary {
+	/*
+	 * Returns a string with the supplied targetLength as it's length value with
+	 * paddings of zeros towards the left or right Supply direction = true for
+	 * padding towards the left
+	 */
+	public static String padWithZeros(String binary, int targetLength, boolean direction) {
+		if (direction)
+			return padWithZerosLeft(binary, targetLength);
+		return padWithZerosRight(binary, targetLength);
+	}
+
+	/*
+	 * Helper function for when the value of the direction parameter is set to
+	 * true.
+	 */
+	public static String padWithZerosLeft(String binary, int targetLength) {
+		while (binary.length() < targetLength) {
+			binary = "0" + binary;
+		}
+		return binary;
+	}
+	/*
+	 * Helper function for when the value of the direction parameter is set to
+	 * false.
+	 */
+
+	public static String padWithZerosRight(String binary, int targetLength) {
+		while (binary.length() < targetLength) {
+			binary += "0";
+		}
+		return binary;
+	}
+
+	/*
+	 * Returns the supplied string of a binary represented value after
+	 * performing a circular shift lift.
+	 */
+	public static String circularShiftLeft(String s) {
+		return s.substring(1, s.length()) + s.charAt(0);
+	}
+
+	/*
+	 * Returns the supplied string of a binary represented value after
+	 * performing a circular shift right.
+	 */
+	public static String circularShiftRight(String s) {
+		return s.charAt(s.length() - 1) + s.substring(0, s.length() - 1);
+	}
+
+	/*
+	 * Returns the obtained value after XOR-ing the two supplied binary
+	 * representations.
+	 */
+	public static String xor(String x, String y) {
+		String z = "";
+		for (int i = 0; i < x.length(); i++) {
+			char c1 = x.charAt(i);
+			char c2 = y.charAt(i);
+			z += (c1 == c2) ? "0" : "1";
+		}
+		return z;
+	}
+
+	/*
+	 * Converts a binary representation to it's corresponding string of bytes.
+	 * Assumes 32-bit blocks with 8-bit words.
+	 */
+	public static String toBytes(String s) {
+		String ret = "";
+		for (int i = 0; i < 4; i++) {
+			int val = Integer.parseInt(s.substring(8 * i, (i + 1) * 8), 2);
+			ret += (char) val;
+		}
+		return ret;
+	}
+
+	/*
+	 * Converts a string of bytes to it's corresponding binary representation.
+	 */
+	public static String toBinary(String s) {
+		byte[] bytes = s.getBytes();
+		String out = "";
+		for (byte b : bytes) {
+			String bString = Integer.toBinaryString(b);
+			while (bString.length() < 8)
+				bString = "0" + bString;
+			out += bString;
+		}
+		return out;
+	}
+}
